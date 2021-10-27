4510989e99bc74461b1c332893785b1dd723a053
David <jobanibusiness@gmail.com>
2021-10-27 14:19:05 -0700

    updated website
    added another zippo lighter - red camo

--- ART109/Project02_FinalSubmission/lighterEmulator.js
+++ ART109/Project02_FinalSubmission/lighterEmulator.js
@@ -1,9 +1,10 @@
 let trigger = 1;
-let img, imgRED_OFF, imgBLUE;
-var redZippoButton, blueZippoButton, realisticButton;
+let img, imgRED_OFF, imgBLUE, imgCAMORED;
+var redZippoButton, blueZippoButton, camoRedZippoButton, realisticButton;
 var realX = 0;
 var redX = -1000;
 var blueX = -1000;
+var camoRedX = -1000;
 var lightX1 = -1000;
 var lightX2 = -1000;
 
@@ -23,6 +24,7 @@ function preload()
   img = loadImage('images/zippoLighter_Off.png');
   imgRED_OFF = loadImage('images/zippoLighter_RED_OFF.png');
   imgBLUE = loadImage('images/zippoLighter_BLUE_OFF.png');
+  imgCAMORED = loadImage('images/zippoLighter_CAMO_RED_OFF.png');
 
   //Sound preload
   zippoOpen = loadSound("sound/zippoOpen.mp3");
@@ -32,6 +34,26 @@ function preload()
 
 function setup()
 {
+
+  //Camo Red Lighter Button changes lighter to camo red
+
+  camoRedZippoButton = new Clickable();
+  camoRedZippoButton.width = 100;
+  camoRedZippoButton.color = "#9c1118";
+  camoRedZippoButton.locate(30, 260);
+  camoRedZippoButton.text = "Camo Lighter";
+
+  camoRedZippoButton.onPress = function()
+  {
+      //zippoOpen.play();
+      camoRedX = 0;
+      redX = -1000;
+      realX = -1000;
+      blueX = -1000;
+      lightX1 = -1000;
+  //    lightX2 = -12;
+  }
+
   //Red Lighter Button changes lighter to red
   redZippoButton = new Clickable();
   redZippoButton.width = 100;
@@ -45,7 +67,9 @@ function setup()
       redX = 0;
       realX = -1000;
       blueX = -1000;
+      camoRedX = -1000;
       lightX1 = -1000;
+
   //    lightX2 = -12;
   }
 
@@ -61,7 +85,9 @@ function setup()
     blueX = 0;
     redX = -1000;
     realX = -1000;
+    camoRedX = -1000;
     lightX1 = -1000;
+
 //    lightX2 = -12;
   }
 
@@ -77,6 +103,7 @@ function setup()
     realX = 0;
     redX = -1000;
     blueX = -1000;
+    camoRedX = -1000;
 //    lightX1 = -12;
     lightX2 = -1000;
   }
@@ -93,6 +120,7 @@ function draw()
   redZippoButton.draw();
   blueZippoButton.draw();
   realisticButton.draw();
+  camoRedZippoButton.draw();
 
   zippoLighter();
 }
@@ -106,7 +134,7 @@ function mousePressed()
     r= 0;
     g = 0;
     b = 0;
-    if(redX == 0 || realX == 0 || blueX == 0 && trigger ==0)
+    if(redX == 0 || realX == 0 || blueX == 0  || camoRedX == 0 && trigger ==0)
     {
       lightX1 = -1000;
       lightX2 = -1000;
@@ -115,6 +143,7 @@ function mousePressed()
     img = loadImage('images/zippoLighter_Off.png');
     imgRED_OFF = loadImage('images/zippoLighter_RED_OFF.png');
     imgBLUE = loadImage('images/zippoLighter_BLUE_OFF.png');
+    imgCAMORED = loadImage('images/zippoLighter_CAMO_RED_OFF.png');
   }
   else
   {
@@ -124,12 +153,12 @@ function mousePressed()
     r= 20;
     g = 20;
     b = 20;
-    if(realX == 0 && redX == -1000 && blueX == -1000)
+    if(realX == 0 && redX == -1000 && blueX == -1000 && camoRedX == -1000)
     {
       lightX1 = -12;
       lightX2 = -1000;
     }
-    else if(redX == 0 || blueX == 0 && realX == -1000)
+    else if(redX == 0 || blueX == 0 || camoRedX ==0 && realX == -1000)
     {
       lightX2 = -12;
       lightX1 = -1000;
@@ -138,6 +167,7 @@ function mousePressed()
     img = loadImage('images/zippoLighter.png');
     imgRED_OFF = loadImage('images/zippoLighter_RED_ON.png');
     imgBLUE = loadImage('images/zippoLighter_BLUE_ON.png');
+    imgCAMORED = loadImage('images/zippoLighter_CAMO_RED_ON.png');
   }
 }
 
@@ -182,6 +212,13 @@ function zippoLighter()
     translate((mouseX - blueX) - 30, mouseY-44);
     scale(.15);
     image(imgBLUE,0,0);
+  pop();
+
+  //Creates Camo Red Lighter
+  push();
+    translate((mouseX - camoRedX) - 30, mouseY-44);
+    scale(.15);
+    image(imgCAMORED,0,0);
   pop();
 
 }
